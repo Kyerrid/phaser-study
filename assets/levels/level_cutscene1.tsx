3e53e11aa21269185418000d1b68501a6cb74a67
Kyerrid <42293851+Kyerrid@users.noreply.github.com>
2018-08-14 16:10:01 +0200

    aggiunta personaggio e animazioni, rework mappa
    aggiunta personaggio e animazioni, rework mappa

--- assets/levels/level_cutscene1.tsx
+++ assets/levels/level_cutscene1.tsx
@@ -1,4 +1,69 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="cutscene1" tilewidth="32" tileheight="32" tilecount="232" columns="29">
- <image source="..\/tilesets\/tilesets_level.png" width="950" height="256"/>
+ <image source="../tilesets/tilesets_level.png" width="950" height="256"/>
+ <tile id="39">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="41">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="32" height="31"/>
+  </objectgroup>
+ </tile>
+ <tile id="154">
+  <objectgroup draworder="index">
+   <object id="2" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="155">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="156">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="157">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="160">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="163">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="166">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="187">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="188">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="32" height="31"/>
+  </objectgroup>
+ </tile>
+ <tile id="193">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
+ <tile id="194">
+  <objectgroup draworder="index">
+   <object id="1" x="0" y="0" width="32" height="32"/>
+  </objectgroup>
+ </tile>
 </tileset>
--- index.js
+++ index.js
@@ -2,6 +2,13 @@ var config = {
     type: Phaser.AUTO,
     width: 800,
     height: 576,
+    physics: {
+         default: 'arcade',
+         arcade: {
+             gravity: { y: 300 },
+             debug: false
+         }
+     },
     scene: {
         preload: preload,
         create: create,
@@ -19,22 +26,78 @@ function preload ()
   this.load.tilemapTiledJSON("cutscene1", "assets/levels/level_cutscene1.json");
   this.load.image('cloud1_1', 'assets/sprites/clouds_1_1.png');
   this.load.image('cloud1_2', 'assets/sprites/clouds_1_2.png');
+
+  this.load.spritesheet('adventurer',
+      'assets/sprites/adventurer/adventurer-v1.5-Sheet.png',
+      { frameWidth: 50, frameHeight: 37 }
+  );
 }
 
 function create ()
 {
 
-  var background = this.add.sprite(400,300,"sky");
-  background.setScale(2.5);
+  background = this.add.sprite(400,300,"sky");
+  background.setScale(4.5);
 
-  var cloud1 = this.add.sprite(500,150,"cloud1_1");
+  cloud1 = this.add.sprite(1800,500,"cloud1_1");
+  cloud2 = this.add.sprite(1500,350,"cloud1_2");
+  cloud3 = this.add.sprite(800,400,"cloud1_1");
+  map = this.make.tilemap({ key: "cutscene1" });
+  tileset = map.addTilesetImage("cutscene1", "cutscene1_tileset");
+  worldLayer = map.createStaticLayer("cutscene1", tileset, 0, 0);
+  worldLayer_nocollision = map.createStaticLayer("cutscene1_nocollide", tileset, 0, 0);
+  worldLayer.setCollisionByExclusion([11,13,126,127,128,1]);
 
-  const map = this.make.tilemap({ key: "cutscene1" });
-  const tileset = map.addTilesetImage("cutscene1", "cutscene1_tileset");
-  const worldLayer = map.createStaticLayer("cutscene1", tileset, 0, 0);
-  worldLayer.setCollisionByProperty({ collides: true });
+  adventurer = this.physics.add.sprite(2300, 710, 'adventurer');
+  adventurer.setScale(1.2);
+  this.physics.add.collider(worldLayer, adventurer);
+
+  this.anims.create({
+      key: 'idle',
+      frames: this.anims.generateFrameNumbers('adventurer', { start: 0, end: 3 }),
+      frameRate: 10,
+      repeat: -1
+  });
+
+  this.anims.create({
+      key: 'move',
+      frames: this.anims.generateFrameNumbers('adventurer', { start: 9, end: 13 }),
+      frameRate: 10,
+      repeat: -1
+  });
+
+  cursors = this.input.keyboard.createCursorKeys();
+    
+  this.cameras.main.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
+  this.cameras.main.startFollow(adventurer);
+
 }
 
 function update ()
 {
+  // Movimento
+  if (cursors.left.isDown)
+  {
+      adventurer.setVelocityX(-160);
+      adventurer.flipX = true;
+      adventurer.anims.play('move', true);
+  }
+  else if (cursors.right.isDown)
+  {
+      adventurer.setVelocityX(160);
+      adventurer.flipX = false;
+      adventurer.anims.play('move', true);
+  }
+  else {
+    adventurer.setVelocityX(0);
+    adventurer.flipX = true;
+    adventurer.anims.play('idle', true);
+  }
+
+  // Salto
+  if (cursors.up.isDown && adventurer.body.onFloor())
+  {
+      adventurer.setVelocityY(-250);
+  }
+
 }
